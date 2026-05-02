d9861229e7f0efa9cf6acf5daf87d10563ee95c3
Vladimir Tchuiev <rmcf126@gmail.com>
2026-05-02 22:53:01 +0300

    Exp: EMA decay 0.99->0.995 [seeded]

--- train.py
+++ train.py
@@ -1819,7 +1819,7 @@ def build_learning_params() -> LearningParameters:
         batch_size=8,
         grad_accumulation=1,
         epochs=2,
-        beta_ema=0.99,
+        beta_ema=0.995,
         gradient_clip=None,
         save_path="saved/",
         amp=False,
