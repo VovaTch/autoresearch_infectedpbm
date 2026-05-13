1d507753d6401569fef046eaf1affd7bf27e090c
Vladimir Tchuiev <rmcf126@gmail.com>
2026-05-14 00:00:59 +0300

    Exp21: decoder dropout 0.1 -> 0.2

--- train.py
+++ train.py
@@ -1988,7 +1988,7 @@ def build_decoder() -> StftDecoder2D:
         dim_add_kernel_add=0,
         num_res_block_conv=2,
         activation_fn=nn.GELU(),
-        dropout=0.1,
+        dropout=0.2,
         dilation_factor=2,
         n_fft=1024,
         hop_length=256,
